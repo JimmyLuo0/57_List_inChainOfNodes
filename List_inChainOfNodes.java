797bd42a77ad92fdae4910c3ce530b0b8c58511d
Jimmy Luo <jluo00@stuy.edu>
2019-03-25 05:07:14 +0900

    completed implementation of linked nodes list

--- List_inChainOfNodes.java
+++ List_inChainOfNodes.java
@@ -13,17 +13,30 @@ public class List_inChainOfNodes{
     /**
       @return the number of elements in this list
      */
-    // public int size() {
-    // }
-
+    public int size() {
+		if (headReference == null)
+			return 0;
+		else return size(headReference.getReferenceToNextNode());
+    }
+	public int size(Node nextNode) {
+		if (nextNode == null)
+			return 1;
+		else return 1 + size(nextNode.getReferenceToNextNode());
+	}
     
      /**
        @return a string representation of this list,
        format:
            # elements [element0,element1,element2,] 
       */
-    // public String toString() {
-    // }
+    public String toString() {
+		String result = ""; 
+		if (headReference == null)
+			result += "No Nodes";
+		else result += headReference.toString();
+		result += " Size: " + size();
+		return result;
+    }
     
     
     /**
@@ -32,6 +45,8 @@ public class List_inChainOfNodes{
       @return true, in keeping with conventions yet to be discussed
      */
      public boolean addAsHead( Object val) {
+		 Node newNode = new Node(val, headReference);		 
+		 headReference = newNode;
         return true;
      }
 }
